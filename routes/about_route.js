56336dc2d9807f2fcedc5ffe60e78ead23b58f77
Faris <faris.223040126@mail.unpas.ac.id>
2025-08-27 11:12:28 +0700

    update

--- routes/about_route.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router();
-const aboutController = require('../controller/about_controller');
-
-router.get('/about', aboutController.getAboutPage)
-router.post('/about', aboutController.postAboutPage)
-
-module.exports = router;
